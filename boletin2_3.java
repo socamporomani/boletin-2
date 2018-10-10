f918769387f59ef478298c64a3062e8a11cb9284
socamporomani <43774853+socamporomani@users.noreply.github.com>
2018-10-10 11:29:05 +0200

    Update boletin2_3.java

--- boletin2_3.java
+++ boletin2_3.java
@@ -1,1 +1,29 @@
+package boletin2_3;
 
+import java.util.Scanner;
+/**
+ *
+ * @author aparcerozas
+ */
+public class Boletin2_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int b100, b20, b5, m1, dinero;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca el número de billetes de 100€:");
+        b100 = teclado.nextInt();
+        System.out.println("Introduzca el número de billetes de 20€:");
+        b20 = teclado.nextInt();
+        System.out.println("Introduzca el número de billetes de 5€:");
+        b5 = teclado.nextInt();
+        System.out.println("Introduzca el número de monedas de 1€:");
+        m1 = teclado.nextInt();
+        dinero = b100*100 + b20*20 + b5*5 + m1;
+        System.out.println("La cantidad de dinero introducida es de " + dinero + "€");
+    }
+    
+}
